282961ae1b29f85bb367ef0fc6bb567d86b2157c
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-04-10 16:09:12 +0200

    frontend added ready for push HTTP requests to the API

---
+++ Frontend/frontend/src/backend/backend.jsx
@@ -0,0 +1,26 @@
+
+const heroImg = 'http://127.0.0.1:8000/static/images/black.png';
+
+
+function Backend() {
+
+  return (
+    <>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Welcome to Backend section </title>
+      </head>
+      <body>
+        <h1> Felicidades acabas de entrar en un renderizado del frontend</h1>
+
+        <h2> A partir de aqui conectamos con la API y empezamos a mandar nuestras peticiones HTTP</h2>
+        
+      </body>
+      </html>
+    </>
+  )
+}
+
+export default Backend
