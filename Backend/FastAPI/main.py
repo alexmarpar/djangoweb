1942ec1eb91c4b3ad4a9ed20972ad10e94bce625
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-04-10 16:19:05 +0200

    corrections to main.py

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI 
 from routers import products,users,jwt_auth_users,basic_auth_users,users_db
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     #docs_url=None, // You can use https://<<example_url>>/docs or https://<<example_url>>/redoc to consult the  requests methods avaliable 
     #redoc_url=None
 )
+
+# http://localhost:5173  -> react (frontend)
+# http://localhost:8000 -> fastApi (backend)
+
+origenes = ["http://localhost:5173", "http://localhost:8000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origenes,
+    allow_credentials=True,
+    allow_methods=["*"],  # O listar ["GET", "POST", ...]
+    allow_headers=["*"],
+)
+
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
